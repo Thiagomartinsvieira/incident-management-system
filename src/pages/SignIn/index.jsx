6ee3780e1c8b0cf98d71b0c02be52467e1b090e2
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-09-02 16:04:04 -0300

    initial

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,10 @@
+
+const SignIn = () => {
+  return (
+    <div>
+        <h1>Login Page</h1>
+    </div>
+  )
+}
+
+export default SignIn
---
+++ src/pages/SingUp/index.jsx
@@ -0,0 +1,10 @@
+
+const SingUp = () => {
+    return (
+      <div>
+          <h1>Registration page</h1>
+      </div>
+    )
+  }
+  
+  export default SingUp
