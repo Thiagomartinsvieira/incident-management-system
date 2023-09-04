5fdb6a7f7d0e0f3add95bb5e078440741a692fb7
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-09-04 20:50:39 -0300

    function to logout user

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,17 @@
-import './Dashboard.css'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/Auth'
 
-const Dashboard = () => {
+export default function Dashboard() {
+  const { logout } = useContext(AuthContext)
+
+  async function handleLogout() {
+    await logout()
+  }
+
   return (
     <div>
       <h1>Dashboard Page</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
-
-export default Dashboard
