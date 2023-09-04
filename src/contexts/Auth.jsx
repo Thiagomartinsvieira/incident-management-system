46e20e6f35d516131a77ef9c24fcd9db56221d71
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-09-04 19:04:04 -0300

    navigating user to dashboard after registering

--- src/contexts/Auth.jsx
+++ src/contexts/Auth.jsx
@@ -3,20 +3,24 @@ import { auth, db } from '../services/firebaseConnection'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 
+import { useNavigate } from 'react-router-dom'
+
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
   const [loadingAuth, setLoadingAuth] = useState(false)
 
-  function signIn(email, password) {
+  const navigate = useNavigate()
+
+  const signIn = (email, password) => {
     console.log(email)
     console.log(password)
     alert('LOGADO COM SUCESSO')
   }
 
   // Cadastrar um novo user
-  async function signUp(email, password, name) {
+  const signUp = async (email, password, name) => {
     setLoadingAuth(true)
 
     await createUserWithEmailAndPassword(auth, email, password)
@@ -37,6 +41,7 @@ function AuthProvider({ children }) {
           setUser(data)
 
           setLoadingAuth(false)
+          navigate('/dashboard')
         })
       })
       .catch((error) => {
