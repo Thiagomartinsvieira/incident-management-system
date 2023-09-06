96159d4990e9d51be2565f407facd63a9768e355
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-09-06 19:01:42 -0300

    creating dashboard page

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,10 @@
 import { useContext } from 'react'
 import { AuthContext } from '../../contexts/Auth'
 import Header from '../../components/Header'
+import Title from '../../components/Title'
 import Nav from '../../components/Nav'
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import './Dashboard.css'
 
@@ -16,8 +19,52 @@ export default function Dashboard() {
     <div>
       <Nav />
       <Header />
-      <h1>Dashboard Page</h1>
-      <button onClick={handleLogout}>Logout</button>
+
+      <div className="content">
+        <Title name="Tickets">
+          <FiMessageSquare size={25} />
+        </Title>
+        <>
+          <Link to="/newTicket" className="new">
+            <FiPlus color="#fff" size={25} />
+            Open New Ticket
+          </Link>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Client</th>
+                <th scope="col">Subject</th>
+                <th scope="col">status</th>
+                <th scope="col">registered in</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="client">Siri Cascudo</td>
+                <td data-label="Subject">suport</td>
+                <td data-label="status">open</td>
+                <td data-label="registered in">08/05/2023</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#3583f6' }}
+                  >
+                    <FiSearch color="#ffff" size={17} />
+                  </button>
+                  <button className="action">
+                    <FiEdit2
+                      color="#ffff"
+                      size={17}
+                      style={{ backgroundColor: '#f6a935' }}
+                    />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      </div>
     </div>
   )
 }
