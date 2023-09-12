28869fcec3b4a153a6ab0215de2215b508dd6259
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2023-09-12 17:32:29 -0300

    add footer to project

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,30 @@
+import './Footer.css'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <div className="content footer-content">
+        <div className="footer-container">
+          <h3>2023 - All rights are free - Developed by Thiago Martins</h3>
+        </div>
+        <div className="footerrc-social">
+          <a
+            href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/"
+            className="social-icon"
+          >
+            <FaLinkedin size={35} color="white" />
+          </a>
+          <a
+            href="https://github.com/Thiagomartinsvieira"
+            className="social-icon"
+          >
+            <FaGithub size={35} color="white" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
