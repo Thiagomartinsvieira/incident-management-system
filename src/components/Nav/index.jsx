da324803d5d67fd338eda22d1e937775a75d190f
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2023-09-12 18:39:08 -0300

    remove FiUser and add profile user photo

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,12 +1,20 @@
 import './Nav.css'
-import { FiUser, FiArrowDownCircle } from 'react-icons/fi'
+import avatarImg from '../../assets/avatar.png'
+import { FiArrowDownCircle } from 'react-icons/fi'
+import { AuthContext } from '../../contexts/Auth'
+import { useContext } from 'react'
 
 const Nav = () => {
+  const { user } = useContext(AuthContext)
+
   return (
     <div className="nav">
       <h2>Help Desk</h2>
       <div className="user-info">
-        <FiUser />
+        <img
+          src={user.avatarUrl === null ? avatarImg : user.avatarUrl}
+          alt="image user"
+        />
         <h3>logged in user</h3>
         <FiArrowDownCircle size={20} />
       </div>
