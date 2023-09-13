c212f5ebd8f9186bf0abcc16dd1d4571f8442b32
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2023-09-12 21:03:16 -0300

    added link when clicking on logo menu

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,13 +3,16 @@ import avatarImg from '../../assets/avatar.png'
 import { FiArrowDownCircle } from 'react-icons/fi'
 import { AuthContext } from '../../contexts/Auth'
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   const { user } = useContext(AuthContext)
 
   return (
     <div className="nav">
-      <h2>Help Desk</h2>
+      <Link to="/dashboard">
+        <h2>Help Desk</h2>
+      </Link>
       <div className="user-info">
         <img
           src={user.avatarUrl === null ? avatarImg : user.avatarUrl}
