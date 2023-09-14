cc78ebe923ea400fbc4902d29ee81c18605daca2
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2023-09-14 16:50:52 -0300

    add page settings

---
+++ src/pages/Settings/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../../components/Nav'
+import Header from '../../components/Header'
+
+const Settings = () => {
+  return (
+    <div>
+      <Nav />
+      <Header />
+    </div>
+  )
+}
+
+export default Settings
