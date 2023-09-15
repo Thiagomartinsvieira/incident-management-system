84144cef65010458c2aa3f016c689d10f5673702
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2023-09-15 19:54:14 -0300

    added button to delete account

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -5,12 +5,16 @@ import { useContext, useState } from 'react'
 import { AuthContext } from '../../contexts/Auth'
 import { FiSettings } from 'react-icons/fi'
 import { toast } from 'react-toastify'
-
 import './Settings.css'
+import { useNavigate } from 'react-router-dom'
 
 const Settings = () => {
   const [darkMode, setDarkMode] = useState(false)
+  const [deleteAccountConfirm, setDeleteAccountConfirm] = useState(false)
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
 
+  const navigate = useNavigate()
   const { user } = useContext(AuthContext)
 
   const handleSubmit = (e) => {
@@ -22,38 +26,71 @@ const Settings = () => {
     setDarkMode(!darkMode)
   }
 
+  const handleDeleteAccount = () => {
+    if (deleteAccountConfirm) {
+      
+
+      navigate('/')
+      toast.success('Your account has been deleted successfully.')
+    } else {
+      setDeleteAccountConfirm(true)
+    }
+  }
+
   return (
     <div>
       <Nav />
       <Header />
-
       <div className={`content ${darkMode ? 'dark-mode' : ''}`}>
         <Title name="Settings">
           <FiSettings size={25} />
         </Title>
-
-        <div className="container">
-          <div className="contact-container">
-            <form
-              className="form-profile form-settings"
-              onSubmit={handleSubmit}
-            >
-              <label>Your Name</label>
-              <input type="text" value={user.nome} readOnly />
-              <label>Enable dark mode</label>
-              <label className="toggle">
-                <input
-                  type="checkbox"
-                  checked={darkMode}
-                  onChange={toggleDarkMode}
-                />
-                <span className="slider"></span>
-              </label>
-              <label>Change Password</label>
-              <input type="password" placeholder="New Password" />
-              <input type="password" placeholder="Confirm New Password" />
-              <input type="submit" value="Submit" />
-            </form>
+        <div className="container container-settings">
+          <form className="form-profile form-settings" onSubmit={handleSubmit}>
+            <label>Your Name</label>
+            <input type="text" value={user.nome} />
+            <label>Enable dark mode</label>
+            <label className="toggle">
+              <input
+                type="checkbox"
+                checked={darkMode}
+                onChange={toggleDarkMode}
+              />
+              <span className="slider"></span>
+            </label>
+            <label>Change Password</label>
+            <input
+              type="password"
+              placeholder="New Password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm New Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <input type="submit" value="Submit" />
+          </form>
+          <div className="delete-account-container">
+            {!deleteAccountConfirm && (
+              <div className="delete-account">
+                <p>
+                  Need to delete your account? This action cannot be undone.
+                </p>
+                <button onClick={handleDeleteAccount}>Delete Account</button>
+              </div>
+            )}
+            {deleteAccountConfirm && (
+              <div className="delete-account-confirm">
+                <p>Are you sure you want to delete your account?</p>
+                <button onClick={handleDeleteAccount}>Yes</button>
+                <button onClick={() => setDeleteAccountConfirm(false)}>
+                  No
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
