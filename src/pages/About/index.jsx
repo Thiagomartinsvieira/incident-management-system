d2e566769259eb8ffd92910738191056b814702d
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2023-09-21 16:49:14 -0300

    added github link

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -4,7 +4,7 @@ import Footer from '../../components/Footer'
 import Title from '../../components/Title'
 import { useContext, useState } from 'react'
 import { AuthContext } from '../../contexts/Auth'
-import { FiGithub, FiLink2, FiLinkedin } from 'react-icons/fi'
+import { FiGithub, FiLinkedin } from 'react-icons/fi'
 import './About.css'
 import ProjectCard from '../../components/ProjectCard'
 
@@ -58,31 +58,33 @@ const About = () => {
             <ProjectCard
               title="Portifolio"
               description="Description"
-              githubLink="Link to gitHub"
-              linkedinLink="Linkedin link"
+              githubLink="https://github.com/Thiagomartinsvieira/portfolio"
+              imageLink="https://portfolio-plum-rho-75.vercel.app/"
               imageSrc="src/assets/portifolio.png"
             />
             <ProjectCard
               title="Auto Vieira"
               description="Version 2 of the Auto Viação Vieira Website: Fictitious project developed with React and TypeScript. Intuitive interface, improved navigation, online ticket purchase, attractive promotions and emphasis on safety and comfort. Demonstration of skills in front-end development and creation of responsive interfaces."
-              githubLink="Link to gitHub"
-              linkedinLink="Linkedin link"
+              githubLink="https://github.com/Thiagomartinsvieira/siteDePassagens"
+              imageLink="https://site-de-passagens.vercel.app/"
               imageSrc="src/assets/auto-vieira.png"
             />
             <ProjectCard
               title="Prime Flix"
               description="Prime-Flix: My first project using an API. A React application that uses The Movie Database (TMDb) API to explore movies and series. Search detailed information, ratings, cast and genres. Personalized recommendations, playlists and viewing progress tracking. An intuitive and user-friendly navigation experience for movie and series enthusiasts."
-              githubLink="Link to gitHub"
+              githubLink="https://github.com/Thiagomartinsvieira/projeto-filmes"
               linkedinLink="Linkedin link"
               imageSrc="src/assets/prime-flix.png"
             />
-            <ProjectCard
-              title="Password Generator"
-              description="Password generator developed with HTML5, CSS3 and JavaScript. The tool allows you to generate personalized passwords with a variable length of characters. The user can adjust the password length using a slider and then generate a new password by clicking the corresponding button. The generated password is displayed on the screen and can be copied with a single click. A practical and secure solution for creating strong passwords."
-              githubLink="Link to gitHub"
-              linkedinLink="Linkedin link"
-              imageSrc="src/assets/password-generator.png"
-            />
+            <a href="">
+              <ProjectCard
+                title="Password Generator"
+                description="Password generator developed with HTML5, CSS3 and JavaScript. The tool allows you to generate personalized passwords with a variable length of characters. The user can adjust the password length using a slider and then generate a new password by clicking the corresponding button. The generated password is displayed on the screen and can be copied with a single click. A practical and secure solution for creating strong passwords."
+                githubLink="https://github.com/Thiagomartinsvieira/Gerador_de_senha"
+                linkedinLink="Linkedin link"
+                imageSrc="src/assets/password-generator.png"
+              />
+            </a>
           </div>
         </div>
       </div>
