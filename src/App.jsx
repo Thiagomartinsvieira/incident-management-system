62ad647ff0aa5b833ad88e232030f4f24828e12b
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-09-26 18:48:07 -0300

    change color slider and slider before in dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import RoutesApp from './routes/routes'
 import AuthProvider from './contexts/Auth'
+import { DarkModeProvider } from './contexts/darkMode'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -10,8 +11,10 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <ToastContainer autoClose={4000} />
-        <RoutesApp />
+        <DarkModeProvider>
+          <ToastContainer autoClose={4000} />
+          <RoutesApp />
+        </DarkModeProvider>
       </AuthProvider>
     </BrowserRouter>
   )
